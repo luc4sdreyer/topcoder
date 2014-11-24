1c79175d1a4d10370fded65900ee354136d4f5e3
luc4sdreyer <luc4sdreyer@gmail.com>
2014-11-24 23:09:04 +0200

    Retried SRM 567

--- thesquarerootdilemma-java/TheSquareRootDilemma.java
+++ thesquarerootdilemma-java/TheSquareRootDilemma.java
@@ -1,20 +1,19 @@
 public class TheSquareRootDilemma {
 
-	public int countPairs(int N, int M) {		
-		int pairs = 0;
-		for (int a = 1; a <= N; a++) {
-			int s = 1;
-			for (int x = 2; x*x <= a; x++) {
-				if (a % (x*x) == 0) {
-					s = x*x;
+	public int countPairs(int N, int M) {
+		int num = 0;
+		for (long a = 1; a <= N; a++) {
+			long r = a;
+			for (long i = 2; i*i <= a; i++) {
+				while ((r % (i*i)) == 0) {
+					r /= i*i;
 				}
 			}
-			int r = a / s;
-			
-			for (int y = 1; y*y*r <= M; y++) {
-				pairs++;
+			for (long i = 1; i*i*r <= M; i++) {
+				num++;
 			}
 		}
-		return pairs;		
+		return num;
 	}
+
 }
