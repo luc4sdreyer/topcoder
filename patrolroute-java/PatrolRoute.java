87b022c27669e6bdf9ee58eae6944e479aa5bf9d
luc4sdreyer <luc4sdreyer@gmail.com>
2014-11-27 08:57:01 +0200

    Retried SRM 542

--- patrolroute-java/PatrolRoute.java
+++ patrolroute-java/PatrolRoute.java
@@ -1,7 +1,13 @@
 public class PatrolRoute {
 
 	public int countRoutes(int X, int Y, int minT, int maxT) {
-		return 0;
+		long num = 0;
+		for (int i = 0; i <= X; i++) {
+			for (int j = minT - i ; j <= maxT - i; j++) {
+				num += (X-j)*(j-1) % 1000000007;
+			}
+		}
+		return (int) num;
 	}
 
 }
