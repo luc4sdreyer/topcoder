9252c739bd0b55df50c48ffe7c54270333608fbd
luc4sdreyer <luc4sdreyer@gmail.com>
2014-11-28 11:01:21 +0200

    Retried SRM 541

--- antsmeet-java/AntsMeet.java
+++ antsmeet-java/AntsMeet.java
@@ -1,44 +1,56 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class AntsMeet {
-
+	public static final int[] dx = {1, 0, -1, 0};
+	public static final int[] dy = {0, 1, 0, -1};
+	public static final int BIG = 10000;
+	
 	public int countAnts(int[] x, int[] y, String direction) {
-		int[] dy = new int[x.length];
-		int[] dx = new int[x.length];
-		for (int i = 0; i < x.length; i++) {
-			if (direction.charAt(i) == 'N') {
-				dy[i]++;
-			} else if (direction.charAt(i) == 'S') {
-				dy[i]--;
-			} else if (direction.charAt(i) == 'E') {
-				dx[i]++;
+		int[] dir = new int[direction.length()];
+		for (int i = 0; i < direction.length(); i++) {
+			if (direction.charAt(i) == 'E') {
+				dir[i] = 0;
+			} else if (direction.charAt(i) == 'N') {
+				dir[i] = 1;
 			} else if (direction.charAt(i) == 'W') {
-				dx[i]--;
+				dir[i] = 2;
+			} else {
+				dir[i] = 3;
 			}
 		}
-		HashSet<Integer> killed = new HashSet<Integer>();
-		for (int t = 0; t <= 4000; t++) {
-			HashSet<Integer> toKill = new HashSet<Integer>();
+		
+		boolean[] dead = new boolean[dir.length];
+		for (int i = 0; i < x.length; i++) {
+			x[i] *= 2;
+			y[i] *= 2;
+		}
+		for (int t = 1; t <= 4001; t++) {			
+			HashMap<Integer, Integer> positions = new HashMap<>();
+			boolean[] toKill = new boolean[dead.length];
 			for (int i = 0; i < x.length; i++) {
-				if (killed.contains(i)) {
-					continue;
-				}
-				for (int j = i+1; j < x.length; j++) {
-					if (killed.contains(j)) {
-						continue;
-					}
-					if (2*x[i] + dx[i]*t == 2*x[j] + dx[j]*t 
-							&& 2*y[i] + dy[i]*t == 2*y[j] + dy[j]*t) {
-						toKill.add(i);
-						toKill.add(j);
+				if (!dead[i]) {
+					int pos = x[i] + dx[dir[i]] * t + (y[i] + dy[dir[i]] * t) * BIG;
+					if (positions.containsKey(pos)) {
+						toKill[i] = true;
+						toKill[positions.get(pos)] = true;
+					} else {
+						positions.put(pos, i);
 					}
 				}
 			}
-			for (int i : toKill) {
-				killed.add(i);
+			for (int i = 0; i < toKill.length; i++) {
+				if (toKill[i]) {
+					dead[i] = true;
+				}
 			}
 		}
-		return x.length - killed.size();
+		int numAlive = 0;
+		for (int i = 0; i < dead.length; i++) {
+			if (!dead[i]) {
+				numAlive++;
+			}
+		}
+		return numAlive;
 	}
 
 }
