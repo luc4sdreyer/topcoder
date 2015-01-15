268f30de7d25f55efe445607c4288a101149ad16
luc4sdreyer <luc4sdreyer@gmail.com>
2015-01-15 21:12:17 +0200

    Added HackerRank Weekly challenges Week 13 part 2

--- HackerRank/src/Solution.java
+++ HackerRank/src/Solution.java
@@ -16,7 +16,130 @@ public class Solution {
 		//System.out.println(taumAndBday(System.in));
 		//System.out.println(sherlockAndAnagrams(System.in));
 		//System.out.println(aSuperHero(System.in));
-		//System.out.println(swapsAndSum(System.in));
+		System.out.println(swapsAndSum(System.in));
+	}
+
+	public static String swapsAndSum(InputStream in) {
+		MyScanner scan = new MyScanner(in);
+		StringBuilder sb = new StringBuilder();
+		int n = scan.nextInt();
+		int q = scan.nextInt();
+		long[] a = new long[n];
+		int[][] queries = new int[q][3];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = scan.nextInt();
+		}
+		for (int i = 0; i < q; i++) {
+			for (int j = 0; j < 3; j++) {
+				queries[i][j] = scan.nextInt();
+			}
+		}
+		
+		SegmentTree st = new SegmentTree(a);
+		for (int i = 0; i < q; i++) {
+			if (queries[i][0] == 1) {
+				for (int j = queries[i][1]-1; j <= queries[i][2]-1; j+=2) {
+					long temp = st.get(j, j);
+					st.set(j, st.get(j+1, j+1));
+					st.set(j+1, temp);
+				}
+			} else {
+				sb.append(st.get(queries[i][1]-1, queries[i][2]-1));
+				sb.append("\n");
+			}
+		}
+		return sb.toString();
+	}
+
+
+	public static class SegmentTree {
+		private long[][] t;
+		private long[] a;
+		private int N;
+		private int n;
+
+		/**
+		 * A segment tree can perform a binary associative function (like sum, min, max) over an interval in O(log n) time. 
+		 * Updates are relatively slow, O(log n)
+		 */
+
+		/**
+		 * This function can be any associative binary function. For example sum, min, max, bitwise and, gcd. 
+		 */
+		protected long function(long a, long b) {
+			return a+b;
+		}
+
+		protected int IDENTITY = 0;
+
+		public SegmentTree(long[] b) {
+			n = (int) (Math.log10(b.length)/Math.log10(2))+1;
+			N = 1 << n;
+			this.a = new long[N];
+			for (int i = 0; i < b.length; i++) {
+				this.a[i] = b[i];
+			}
+			t = new long[N][n+1];
+			for (int x = 0; x < N; x++) {
+				t[x][0] = a[x];
+			}
+			for (int y = 1; y <= n; y++) {
+				for (int x = 0; x < N; x+=(1<<y)) {
+					t[x][y] = function(t[x][y-1], t[x+(1<<(y-1))][y-1]);
+				}
+			}
+		}
+
+		public void set(int x, long v) {
+			t[x][0] = a[x] = v;
+			for (int y = 1; y <= n; y++) {
+				int xx = x-(x&((1<<y)-1));
+				t[xx][y] = function(t[xx][y-1], t[xx+(1<<(y-1))][y-1]);
+			}
+		}
+
+		/**
+		 * Get the function over the interval [a, b].
+		 */
+		public long get(int i, int j) {
+			long res = IDENTITY;
+			int h = 0; j++;
+			while (i+(1<<h) <= j) {
+				while ((i&((1<<(h+1))-1)) == 0 && i+(1<<(h+1)) <= j) h++;
+				res = function(res, t[i][h]);
+				i += (1<<h);
+			}
+			while (i < j) {
+				while (i+(1<<h) > j) h--;
+				res = function(res, t[i][h]);
+				i += (1<<h);
+			}
+			return res;
+		}
+	}
+
+	public static String aSuperHero(InputStream in) {
+		MyScanner scan = new MyScanner(in);
+		StringBuilder sb = new StringBuilder();
+		int t = scan.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = scan.nextInt();
+			int m = scan.nextInt();
+			int[][][] costReward = new int[n][m][2];
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < m; k++) {
+					costReward[j][k][0] = scan.nextInt();
+				}
+			}
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < m; k++) {
+					costReward[j][k][1] = scan.nextInt();
+				}
+			}
+			sb.append(aSuperHero(n, m, costReward));
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 	public static final int empty = 1000000;
